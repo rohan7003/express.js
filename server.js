6a169b85787cb16b881f01720a4ae1b5615bc4da
Rohan Patil <146317921+rohan7003@users.noreply.github.com>
2024-02-29 12:22:38 +0000

    done

--- server.js
+++ server.js
@@ -15,6 +15,40 @@ app.get("/todos",(req,res)=>{
     res.status(200).send(toDoList)
 })
 
+app.post("/todos",(req,res)=>{
+    let newDoItem = req.body.item;
+    toDoList.push(newDoItem);
+    res.status(201).send({
+        message: "The task was added succesfully"
+    })
+})
+
+app.delete("/todos",(req,res)=>{
+   var itemToDelete = req.body.item;
+
+   toDoList.find((elem,index)=>{
+    if(elem === itemToDelete){
+        toDoList.splice(index, 1);
+    }
+   })
+   res.status(204).send({
+    message: `Deleted item ${req.body.item}`
+   })
+})
+
+
+//put patch
+app.all("/todos",(req,res)=>{
+    res.status(501).send({
+        message: "Not yet implemented"
+    })
+})
+
+app.all("*",(req,res)=>{
+    res.status(404).send({
+        message: "page not found"
+    })
+})
 app.listen(port,()=>{
     console.log(`NodeJs with Experss started running succesfully on port ${port}`)
 })
